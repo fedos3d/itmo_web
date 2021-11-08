0fbfbcb39fd909b74e23a101a2faba213c229d24
Fedor Anikeev <fedos3d@gmail.com>
2021-11-08 15:08:00 +0300

    Added memes gallery in my_portfolio.html and fixed js code for nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,24 @@
-var before_loadtime = new Date().getTime();
+let before_loadtime = new Date().getTime();
 window.onload = function () {
-    var aftr_loadtime = new Date().getTime();
-    var loadTime = (aftr_loadtime - before_loadtime) / 1000
-    if (document.location.href.indexOf("my_portfolio") > -1) {
-        var curelem = document.getElementById("portfolio")
+    let aftr_loadtime = new Date().getTime();
+    let loadTime = (aftr_loadtime - before_loadtime) / 1000
+    if (document.getElementById("portfolio").getAttribute("data-nav") === "true") {
+        let curelem = document.getElementById("portfolio")
         curelem.style.color = "red"
         curelem.style.border = "solid"
     }
-    else if (document.location.href.indexOf("index") > -1) {
-        var curelem = document.getElementById("home")
+    else if (document.getElementById("home").getAttribute("data-nav") === "true") {
+        let curelem = document.getElementById("home")
         curelem.style.color = "red"
         curelem.style.border = "solid"
     }
-    else if (document.location.href.indexOf("my_achievements") > -1) {
-        var curelem = document.getElementById("achiv")
+    else if (document.getElementById("achiv").getAttribute("data-nav") === "true") {
+        let curelem = document.getElementById("achiv")
         curelem.style.color = "red"
         curelem.style.border = "solid"
     }
-    var element = document.createElement("div").appendChild(document.createElement('p').appendChild(document.createTextNode("Load time: " + loadTime)))
+    let textNode = document.createTextNode("Load time: " + loadTime)
+    let pNode = document.createElement('p').appendChild(textNode)
+    let element = document.createElement("div").appendChild(pNode)
     document.body.appendChild(element)
 }
