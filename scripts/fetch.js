992cb6e3da3fb2ea09338079bf9a5f3984fef113
Fedor Anikeev <fedos3d@gmail.com>
2021-12-06 14:00:49 +0300

    Added error log alert

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,4 +1,12 @@
-const movieApiMovies = () => {
+function filter (i) {
+    let num = 5
+    if ((num % 2) == 0) {
+        return i.id <= 5
+    } else {
+        return i.id >= 5
+    }
+}
+const fetchAPI = () => {
     // var temp = document.getElementsByTagName("template")[0];
     // var clon = temp.content.cloneNode(true);
     let loader = '<div id="preloader">\n' +
@@ -6,14 +14,15 @@ const movieApiMovies = () => {
         '</div>'
     document.getElementById('results').innerHTML = loader;
     fetch('https://jsonplaceholder.typicode.com/users')
-        .then((response) => {
-            if(!response.ok) throw new Error(response.status);
-            else return response.json();
+        .then((response) => response.json())
+        .then(json => {
+            console.log(json);
+            document.getElementById('results').innerHTML = json
         })
-        .then(json => console.log(json) (
-            document.getElementById('results').innerHTML = json))
         .catch((error) => {
-        console.log('error: ' + error);
-    });
+            console.log('error: ' + error);
+            alert("bad request");
+            document.getElementById('results').innerHTML = '';
+        });
 };
-movieApiMovies()
+fetchAPI()
