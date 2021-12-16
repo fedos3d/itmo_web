3be2d6f0238d1b9b8d3f51d6a1ec9b5290b14763
Fedor Anikeev <fedos3d@gmail.com>
2021-12-16 22:13:51 +0300

    Added commas to js

--- scripts/constructor.js
+++ scripts/constructor.js
@@ -1,3 +1,6 @@
+// import { hello } from './schedule_data.js';
+
+
 const WeekType = Object.freeze({
     odd: "нечет",
     even: "чет",
@@ -12,7 +15,6 @@ const WeekdaysRU = Object.freeze({
     Thursday: 'Четверг',
     Friday: 'Пятница',
     Saturday: 'Суббота'
-
 });
 const WeekdaysEN = Object.freeze({
     Monday:   'Monday',
@@ -21,7 +23,6 @@ const WeekdaysEN = Object.freeze({
     Thursday: 'Thursday',
     Friday: 'Friday',
     Saturday: 'Saturday'
-
 });
 
 
@@ -29,8 +30,8 @@ class WorkDay {
     classes;
     weekday;
     constructor(weekday, classes){
-        this.weekday = weekday
-        this.classes = classes
+        this.weekday = weekday;
+        this.classes = classes;
     }
 }
 
@@ -66,176 +67,166 @@ class Zan {
 
 //let's create out schedule table:
 //Monday:
-var monday = []
+var monday = [];
 var Linux = new Zan(new StudyTime("8:20", "9:50"), WeekType.even, '325',
     'Администрирование в ОС Linux/Linux operating system administration', 'Лабораторные занятия',
-    'Очно - дистанционный ', 'Шараева Кристина Витальевна', 'M33031')
-monday.push(Linux)
-var weekdays = []
-var weekday = new WorkDay(WeekdaysRU.Monday, monday)
+    'Очно - дистанционный ', 'Шараева Кристина Витальевна', 'M33031');
+monday.push(Linux);
+var weekdays = [];
+var weekday = new WorkDay(WeekdaysRU.Monday, monday);
 
 var Linux = new Zan(new StudyTime("10:00", "11:30"), WeekType.even, '325',
     'Администрирование в ОС Linux/Linux operating system administration', 'Лабораторные занятия',
-    'Очно - дистанционный ', 'Шараева Кристина Витальевна', 'M33031')
-monday.push(Linux)
+    'Очно - дистанционный ', 'Шараева Кристина Витальевна', 'M33031');
+monday.push(Linux);
 var ML = new Zan(new StudyTime("11:40", "13:10"), WeekType.all, '285',
     'Машинное обучение', 'Лекции',
-    'Очно - дистанционный ', 'Хлопотов Максим Валерьевич', 'M33031')
-monday.push(ML)
+    'Очно - дистанционный ', 'Хлопотов Максим Валерьевич', 'M33031');
+monday.push(ML);
 var WEB = new Zan(new StudyTime("13:30", "15:00"), WeekType.odd, '236',
     'Web-программирование', 'Лабораторные занятия',
-    'Очно - дистанционный ', '-', 'M33031')
-monday.push(WEB)
+    'Очно - дистанционный ', '-', 'M33031');
+monday.push(WEB);
 var WEB = new Zan(new StudyTime("15:20", "16:50"), WeekType.odd, '236',
     'Web-программирование', 'Лабораторные занятия',
-    'Очно - дистанционный ', '-', 'M33031')
-monday.push(WEB)
-weekdays.push(weekday)
+    'Очно - дистанционный ', '-', 'M33031');
+monday.push(WEB);
+weekdays.push(weekday);
 //Let's fill out wednesday:
-var wednesday = []
-var weekday = new WorkDay(WeekdaysRU.Wednesday, wednesday)
+var wednesday = [];
+var weekday = new WorkDay(WeekdaysRU.Wednesday, wednesday);
 var WEB = new Zan(new StudyTime("11:40", "13:10"), WeekType.all, '100/1(усл) ',
     'Web-программирование', 'Лекции',
-    'Очно - дистанционный ', 'Приискалов Роман Андреевич', 'M33031')
+    'Очно - дистанционный ', 'Приискалов Роман Андреевич', 'M33031');
 wednesday.push(WEB);
 var ML = new Zan(new StudyTime("13:30", "15:00"), WeekType.odd, '239',
     'Машинное обучение', 'Лабораторные занятия',
-    'Очно - дистанционный ', 'Хлопотов Максим Валерьевич', 'M33031')
+    'Очно - дистанционный ', 'Хлопотов Максим Валерьевич', 'M33031');
 wednesday.push(ML);
 var ML = new Zan(new StudyTime("15:20", "16:50"), WeekType.odd, '239',
     'Машинное обучение', 'Лабораторные занятия',
-    'Очно - дистанционный ', 'Хлопотов Максим Валерьевич', 'M33031')
-wednesday.push(ML)
-weekdays.push(weekday)
+    'Очно - дистанционный ', 'Хлопотов Максим Валерьевич', 'M33031');
+wednesday.push(ML);
+weekdays.push(weekday);
 //Let's fill out thursday
-var thursday = []
-var weekday = new WorkDay(WeekdaysRU.Thursday, thursday)
+var thursday = [];
+var weekday = new WorkDay(WeekdaysRU.Thursday, thursday);
 var EN = new Zan(new StudyTime("08:20", "09:50"), WeekType.all, '3212',
     'Иностранный язык в профессиональной деятельности', 'Практические занятия',
-    'Очно - дистанционный ', 'Немец Мария Сергеевна', 'АЯ-3IT-C2/1')
-thursday.push(EN)
+    'Очно - дистанционный ', 'Немец Мария Сергеевна', 'АЯ-3IT-C2/1');
+thursday.push(EN);
 var EN = new Zan(new StudyTime("10:00", "11:30"), WeekType.all, '3212',
     'Иностранный язык в профессиональной деятельности', 'Практические занятия',
-    'Очно - дистанционный ', 'Немец Мария Сергеевна', 'АЯ-3IT-C2/1')
-thursday.push(EN)
+    'Очно - дистанционный ', 'Немец Мария Сергеевна', 'АЯ-3IT-C2/1');
+thursday.push(EN);
 var Linux = new Zan(new StudyTime("13:30", "15:00"), WeekType.all, '100/1(усл) ',
     'Администрирование в ОС Linux/Linux operating system administration', 'Лекции',
-    'Очно - дистанционный ', 'Маятин Александр Владимирович', 'M33031')
-thursday.push(Linux)
+    'Очно - дистанционный ', 'Маятин Александр Владимирович', 'M33031');
+thursday.push(Linux);
 var UML = new Zan(new StudyTime("15:20", "16:50"), WeekType.all, '331',
     'Анализ и проектирование на UML', 'Лекции',
-    'Очно - дистанционный ', 'Хлопотов Максим Валерьевич', 'M33031')
-thursday.push(UML)
-weekdays.push(weekday)
+    'Очно - дистанционный ', 'Хлопотов Максим Валерьевич', 'M33031');
+thursday.push(UML);
+weekdays.push(weekday);
 //Let's fill out friday
-var friday = []
-var weekday = new WorkDay(WeekdaysRU.Friday, friday)
+var friday = [];
+var weekday = new WorkDay(WeekdaysRU.Friday, friday);
 var UML = new Zan(new StudyTime("10:00", "11:30"), WeekType.odd, '236',
     'Анализ и проектирование на UML', 'Лабораторные занятия',
-    'Очно - дистанционный ', 'Хлопотов Максим Валерьевич', 'M33031')
-friday.push(UML)
+    'Очно - дистанционный ', 'Хлопотов Максим Валерьевич', 'M33031');
+friday.push(UML);
 var UML = new Zan(new StudyTime("11:40", "13:10"), WeekType.odd, '236',
     'Анализ и проектирование на UML', 'Лабораторные занятия',
-    'Очно - дистанционный ', 'Хлопотов Максим Валерьевич', 'M33031')
-friday.push(UML)
+    'Очно - дистанционный ', 'Хлопотов Максим Валерьевич', 'M33031');
+friday.push(UML);
 var HISTORY = new Zan(new StudyTime("11:40", "13:10"), WeekType.even, '332',
     'История (Наука и техника в истории цивилизации)', 'Лекции',
-    'Очно - дистанционный ', 'Васильев Андрей Владимирович', 'ИНТ 6')
-friday.push(HISTORY)
+    'Очно - дистанционный ', 'Васильев Андрей Владимирович', 'ИНТ 6');
+friday.push(HISTORY);
 var HISTORY = new Zan(new StudyTime("13:30", "15:00"), WeekType.even, '429',
     'История (Наука и техника в истории цивилизации)', 'Практические занятия',
-    'Очно - дистанционный ', 'Васильев Андрей Владимирович', 'ИНТ 6.1')
-friday.push(HISTORY)
-weekdays.push(weekday)
+    'Очно - дистанционный ', 'Васильев Андрей Владимирович', 'ИНТ 6.1');
+friday.push(HISTORY);
+weekdays.push(weekday);
 //Let's fill out saturday
-var saturday = []
-var weekday = new WorkDay(WeekdaysRU.Saturday, saturday)
+var saturday = [];
+var weekday = new WorkDay(WeekdaysRU.Saturday, saturday);
 var PRIMAT = new Zan(new StudyTime("11:40", "13:10"), WeekType.even, '3408',
     'Прикладная математика', 'Лекции',
-    'Очно - дистанционный ', 'Москаленко Мария Александровна', 'M33031')
-saturday.push(PRIMAT)
+    'Очно - дистанционный ', 'Москаленко Мария Александровна', 'M33031');
+saturday.push(PRIMAT);
 var PRIMAT = new Zan(new StudyTime("13:30", "15:00"), WeekType.even, '2202',
     'Прикладная математика', 'Лекции',
-    'Очно - дистанционный ', 'Москаленко Мария Александровна', 'M33031')
-saturday.push(PRIMAT)
+    'Очно - дистанционный ', 'Москаленко Мария Александровна', 'M33031');
+saturday.push(PRIMAT);
 var PRIMAT = new Zan(new StudyTime("15:20", "16:50"), WeekType.odd, '1513',
     'Прикладная математика', 'Лабораторные занятия',
-    'Очно - дистанционный ', 'Москаленко Мария Александровна', 'M33031')
-saturday.push(PRIMAT)
+    'Очно - дистанционный ', 'Москаленко Мария Александровна', 'M33031');
+saturday.push(PRIMAT);
 var PRIMAT = new Zan(new StudyTime("17:00", "18:30"), WeekType.odd, '1513',
     'Прикладная математика', 'Лабораторные занятия',
-    'Очно - дистанционный ', 'Москаленко Мария Александровна', 'M33031')
-saturday.push(PRIMAT)
-weekdays.push(weekday)
+    'Очно - дистанционный ', 'Москаленко Мария Александровна', 'M33031');
+saturday.push(PRIMAT);
+weekdays.push(weekday);
 
 
 // input.addEventListener('input', submit);
 function loadLocalStorageData() {
     var lang = window.localStorage.getItem('language');
     var days = window.localStorage.getItem('days');
-    var storedChecks = []
+    var storedChecks = [];
     storedChecks = JSON.parse(window.localStorage.getItem("checks"));
-    // console.log("loaded from storage: " + storedChecks.length)
-    var arr = []
-    arr.push(lang)
-    arr.push(days)
-    arr.push(storedChecks)
+    var arr = [];
+    arr.push(lang);
+    arr.push(days);
+    arr.push(storedChecks);
     return arr;
 }
 
 function formsubmit() {
-    let days = document.getElementById("amount_of_days").value
+    let days = document.getElementById("amount_of_days").value;
     let lang;
     let langs = document.getElementsByName("lang");
     for (var i = 0, length = langs.length; i < length; i++) {
         if (langs[i].checked) {
-            // do whatever you want with the checked radio
-            // alert(langs[i].value);
-            lang = langs[i].value
-
-            // only one radio can be logically checked, don't check the rest
+            lang = langs[i].value;
             break;
         }
     }
     var checkedBoxes = document.querySelectorAll('input[name=column]:checked');
-    // console.log("days: " + days)
-    // console.log("lang: " + lang)
-    // console.log("checked: " + checkedBoxes)
-    // console.log("length fo checked: " + checkedBoxes.length)
-    var checked = []
+    var checked = [];
     for (var i = 0; i < checkedBoxes.length; i++) {
-        // console.log(checkedBoxes[i].value)
-        checked.push(checkedBoxes[i].value)
+        checked.push(checkedBoxes[i].value);
     }
-    drawTable(lang, days, checked)
-    updateLocalStorageData(lang, days, checked)
+    drawTable(lang, days, checked);
+    updateLocalStorageData(lang, days, checked);
 }
 
 function updateLocalStorageData(lang, days, storedChecks) {
 
-    window.localStorage.setItem("language", lang)
-    window.localStorage.setItem("days", days)
-    window.localStorage.setItem("checks", JSON.stringify(storedChecks))
+    window.localStorage.setItem("language", lang);
+    window.localStorage.setItem("days", days);
+    window.localStorage.setItem("checks", JSON.stringify(storedChecks));
 }
 
 function replaceChecked(str) {
     switch (str) {
         case "format":
-            return "Формат занятий"
+            return "Формат занятий";
         case "tutor":
-            return "Преподаватель"
+            return "Преподаватель";
         case "group":
-            return "Группа"
+            return "Группа";
         case "lessontype":
-            return "Вид занятий"
+            return "Вид занятий";
         case "time":
-            return "Время занятий"
+            return "Время занятий";
         case "week":
-            return "Неделя"
+            return "Неделя";
         case "classroom":
-            return "Аудитория"
+            return "Аудитория";
         case "name":
-            return "Дисциплина"
+            return "Дисциплина";
     }
 }
 
@@ -243,99 +234,96 @@ function replaceChecked(str) {
 function drawTable(lang, days, storedChecks) {
     var myNode = document.getElementById("results");
     myNode.innerHTML = '';
-    let bigtable = document.createElement('table')
+    let bigtable = document.createElement('table');
     for (var i = 0; i < days && i < weekdays.length; i++) {
-        let newTable = document.createElement('tbody')
-        var headerrow = document.createElement('tr')
-        var el = document.createElement('td')
-        el.appendChild(document.createTextNode(weekdays[i].weekday))
-        el.setAttribute("class", "daytag")
-        headerrow.appendChild(el)
-        newTable.appendChild(headerrow)
-        var rowone = document.createElement('tr')
+        let newTable = document.createElement('tbody');
+        var headerrow = document.createElement('tr');
+        var el = document.createElement('td');
+        el.appendChild(document.createTextNode(weekdays[i].weekday));
+        el.setAttribute("class", "daytag");
+        headerrow.appendChild(el);
+        newTable.appendChild(headerrow);
+        var rowone = document.createElement('tr');
         for (var j = 0; j < storedChecks.length; j++) {
-            el = document.createElement('th')
-            el.appendChild(document.createTextNode(replaceChecked(storedChecks[j])))
-            rowone.appendChild(el)
-            // rowone.appendChild(document.createElement('td').appendChild(document.createTextNode(replaceChecked(storedChecks[j]))));
+            el = document.createElement('th');
+            el.appendChild(document.createTextNode(replaceChecked(storedChecks[j])));
+            rowone.appendChild(el);
         }
-        newTable.appendChild(rowone)
+        newTable.appendChild(rowone);
         for (var j = 0; j < weekdays[i].classes.length; j++) {
-            var row = document.createElement('tr')
+            var row = document.createElement('tr');
             var el;
             if (storedChecks.includes("time")) {
-                // row.appendChild(document.createElement('td')).appendChild(document.createTextNode(weekdays[i].classes[j].time.start + "->" + weekdays[i].classes[j].time.end)))
-                el = document.createElement('td')
-                el.appendChild(document.createTextNode(weekdays[i].classes[j].time.start + "->" + weekdays[i].classes[j].time.end))
-                row.appendChild(el)
+                el = document.createElement('td');
+                el.appendChild(document.createTextNode(weekdays[i].classes[j].time.start + "->" + weekdays[i].classes[j].time.end));
+                row.appendChild(el);
             }
             if (storedChecks.includes("week")) {
-                el = document.createElement('td')
-                el.appendChild(document.createTextNode(weekdays[i].classes[j].week))
-                row.appendChild(el)
+                el = document.createElement('td');
+                el.appendChild(document.createTextNode(weekdays[i].classes[j].week));
+                row.appendChild(el);
             }
             if (storedChecks.includes("classroom")) {
-                el = document.createElement('td')
-                el.appendChild(document.createTextNode(weekdays[i].classes[j].classroom))
-                row.appendChild(el)
+                el = document.createElement('td');
+                el.appendChild(document.createTextNode(weekdays[i].classes[j].classroom));
+                row.appendChild(el);
             }
-            el = document.createElement('td')
-            el.appendChild(document.createTextNode(weekdays[i].classes[j].discipline_name))
-            row.appendChild(el)
+            el = document.createElement('td');
+            el.appendChild(document.createTextNode(weekdays[i].classes[j].discipline_name));
+            row.appendChild(el);
             if (storedChecks.includes("lessontype")) {
-                el = document.createElement('td')
-                el.appendChild(document.createTextNode(weekdays[i].classes[j].type))
-                row.appendChild(el)
+                el = document.createElement('td');
+                el.appendChild(document.createTextNode(weekdays[i].classes[j].type));
+                row.appendChild(el);
             }
             if (storedChecks.includes("format")) {
-                el = document.createElement('td')
-                el.appendChild(document.createTextNode(weekdays[i].classes[j]._format))
-                row.appendChild(el)
+                el = document.createElement('td');
+                el.appendChild(document.createTextNode(weekdays[i].classes[j]._format));
+                row.appendChild(el);
             }
             if (storedChecks.includes("tutor")) {
-                el = document.createElement('td')
-                el.appendChild(document.createTextNode(weekdays[i].classes[j].tutor))
-                row.appendChild(el)
+                el = document.createElement('td');
+                el.appendChild(document.createTextNode(weekdays[i].classes[j].tutor));
+                row.appendChild(el);
             }
             if (storedChecks.includes("group")) {
-                el = document.createElement('td')
-                el.appendChild(document.createTextNode(weekdays[i].classes[j]._group))
-                row.appendChild(el)
+                el = document.createElement('td');
+                el.appendChild(document.createTextNode(weekdays[i].classes[j]._group));
+                row.appendChild(el);
             }
-            newTable.appendChild(row)
+            newTable.appendChild(row);
         }
-        bigtable.appendChild(newTable)
+        bigtable.appendChild(newTable);
     }
-    document.getElementById("results").appendChild(bigtable)
+    document.getElementById("results").appendChild(bigtable);
 }
 function tickChecked () {
-    var arr = loadLocalStorageData()
-    lang = arr[0]
-    days = arr[1]
-    storedChecks = arr[2]
+    var arr = loadLocalStorageData();
+    lang = arr[0];
+    days = arr[1];
+    storedChecks = arr[2];
     storedChecks.forEach(function (chec) {
         document.querySelectorAll("#columns_to_show input").forEach(function (el) {
             if (el.value === chec) {
-                el.setAttribute("checked", "checked")
+                el.setAttribute("checked", "checked");
             }
         })
     })
-    // console.log(days)
     document.querySelectorAll("#dayamount input").forEach( function (el) {
-        el.value = days
+        el.value = days;
     })
 }
 
 //here we start our html page:
 
-let lang
-let days
-let storedChecks
-var arr = loadLocalStorageData()
-lang = arr[0]
-days = arr[1]
-storedChecks = arr[2]
-tickChecked()
+let lang;
+let days;
+let storedChecks;
+var arr = loadLocalStorageData();
+lang = arr[0];
+days = arr[1];
+storedChecks = arr[2];
+tickChecked();
 if (storedChecks !== undefined) {
-    drawTable(lang, days, storedChecks)
+    drawTable(lang, days, storedChecks);
 }
--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,16 +1,16 @@
 function filter (i) {
-    let max = 20
-    let min = 5
+    let max = 20;
+    let min = 5;
     let num = Math.floor(Math.random() * (max - min) + min);
-    console.log(num)
+    console.log(num);
     if ((i.id % num) === 0) {
-        return i.id
+        return i.id;
     }
 }
 const fetchAPI = () => {
     let loader = '<div id="preloader">\n' +
         '  <div id="loader"></div>\n' +
-        '</div>'
+        '</div>';
     document.getElementById('results').innerHTML = loader;
     fetch('https://jsonplaceholder.typicode.com/comments')
         .then((response) => response.json())
@@ -21,23 +21,23 @@ const fetchAPI = () => {
             json.filter(filter).forEach((item) => {
 
                 let div = document.createElement('div');
-                div.setAttribute("class", "comment")
+                div.setAttribute("class", "comment");
 
                 let h4 = document.createElement("h4");
                 let name = document.createTextNode(item.name);
 
                 let id = document.createElement("span");
-                id.setAttribute("class", "comment_id")
+                id.setAttribute("class", "comment_id");
                 let comment_id = document.createTextNode(" comment_id: " + item.postId);
                 id.appendChild(comment_id);
 
-                let mailBox = document.createElement("span")
-                mailBox.setAttribute("class", "mailbox")
-                let mailText = document.createTextNode(" " + item.email)
+                let mailBox = document.createElement("span");
+                mailBox.setAttribute("class", "mailbox");
+                let mailText = document.createTextNode(" " + item.email);
                 mailBox.appendChild(mailText);
 
                 h4.appendChild(name);
-                h4.appendChild(mailBox)
+                h4.appendChild(mailBox);
                 h4.appendChild(id);
 
                 div.appendChild(h4);
@@ -46,7 +46,7 @@ const fetchAPI = () => {
                 let text = document.createTextNode(item.body);
 
                 paragraph.appendChild(text);
-                div.appendChild(paragraph)
+                div.appendChild(paragraph);
 
                 result.appendChild(div);
             });
@@ -57,4 +57,4 @@ const fetchAPI = () => {
             document.getElementById('results').innerHTML = '';
         });
 };
-fetchAPI()
+fetchAPI();
