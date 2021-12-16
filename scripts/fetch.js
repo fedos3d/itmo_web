6b148b0db402aaec652bb1664aa8c7de09e6da73
Fedor Anikeev <fedos3d@gmail.com>
2021-12-16 22:03:41 +0300

    Added email field to comment section

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -19,20 +19,35 @@ const fetchAPI = () => {
             let result = document.getElementById('results');
             result.innerHTML = '';
             json.filter(filter).forEach((item) => {
+
                 let div = document.createElement('div');
                 div.setAttribute("class", "comment")
+
                 let h4 = document.createElement("h4");
-                let id = document.createElement("span");
                 let name = document.createTextNode(item.name);
-                let comment_id = document.createTextNode(", comment_id: " + item.postId);
+
+                let id = document.createElement("span");
+                id.setAttribute("class", "comment_id")
+                let comment_id = document.createTextNode(" comment_id: " + item.postId);
                 id.appendChild(comment_id);
+
+                let mailBox = document.createElement("span")
+                mailBox.setAttribute("class", "mailbox")
+                let mailText = document.createTextNode(" " + item.email)
+                mailBox.appendChild(mailText);
+
                 h4.appendChild(name);
+                h4.appendChild(mailBox)
                 h4.appendChild(id);
+
                 div.appendChild(h4);
+
                 let paragraph = document.createElement('p');
                 let text = document.createTextNode(item.body);
+
                 paragraph.appendChild(text);
                 div.appendChild(paragraph)
+
                 result.appendChild(div);
             });
         })
